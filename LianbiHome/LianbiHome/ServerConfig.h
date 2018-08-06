8e9986d91c88729052c1ce6bd9b64744ce3a80ec
admin <775860548@qq.com>
2018-08-06 15:45:52 +0800

    图片

---
+++ LianbiHome/LianbiHome/ServerConfig.h
@@ -0,0 +1,25 @@
+//
+//  ServerConfig.h
+//  kyzx
+//
+//  Created by simon on 16/4/24.
+//  Copyright © 2016年 zhiyuan. All rights reserved.
+//
+
+#ifndef ServerConfig_h
+#define ServerConfig_h
+
+
+/*
+ *-------------------------
+ * 服务器相关配置
+ *-------------------------
+ */
+
+//服务器ip或域名
+#define SERVER_ADDRESS_ABSOLUTE_PATH       ("http://api.456mobi.com/api/?s=home/")
+
+//相对路径
+
+
+#endif /* ServerConfig_h */
